5c6eff8dd66cdb30bc761d639076839c0bb8bb60
AliSnickers <alisnickers@gmail.com>
2025-08-22 13:31:28 -0400

    sign in, dashboard, signout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router';
 
 const Dashboard = () => {
+    const { session, signOut } = UserAuth();
+        const navigate = useNavigate();
+
+    const handleSignOut = async (e) => {
+        e.preventDefault()
+        try {
+            await signOut()
+            navigate('/')
+        }
+    }
+
     return (
-        <div>Dashboard</div>
+        <div>
+            <h1>Dashboard</h1>
+            <h2>Welcome, {session?.user?.email}</h2>
+        <div>
+            <p onClick={handleSignOut} className='hover:cursor-pointer border inline-block px-4 py-3 mt-4'>Sign Out</p>
+        </div>
+        </div>
     )
 
 }
