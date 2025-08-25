7a99e57648f89bd1d82aaa5eb8341f1d68277ba9
AliSnickers <alisnickers@gmail.com>
2025-08-25 09:30:37 -0400

    fixing image

--- src/header.jsx
+++ src/header.jsx
@@ -1,9 +1,9 @@
  export default function Header() {
   return (
     <header className="w-full bg-#5d928a text-white p-4">
-      <div className="container mx-auto flex justify-left items-left p-4">
+      <div className="mx-auto flex justify-left items-left p-4">
         <img
-            className="h-48 w-full object-cover md:h-full md:w-48"
+            className="h-48 w-full object-contain md:h-full md:w-48"
             src="nomad.png"
             alt="Nomad Home"
           />
